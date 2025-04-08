41546c21f434e40e38e26d8b86265c16c96f5929
Jimnezoh <jimmaina100@gmail.com>
2025-04-08 09:38:11 +0300

    modified the movie card

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,31 @@
-import React from "react";
+    import React from "react";
 
 const MovieCard = ({movie : 
-    {title, poster_path, overview, release_date, vote_average}
+    {title, poster_path, overview, release_date, vote_average,original_language}
 }) => {
     return (
         <div  className="movie-card">
             <img 
-            src= {poster_path ? `http://image.tmdb.org/t/p/w500/ ${poster_path}` : '/No-Poster.png'}
-            />
-            {/* <p className='text-white'>{title}</p> */}
+            src= {poster_path ? `http://image.tmdb.org/t/p/w500/${poster_path}` : '/No-Poster.png'}
+            alt={title}/>
+            <div className="mt-4">
+                <h3>{title}</h3>
+            </div>
+            <div className="content">
+                <div className="rating">
+                    <img src="star.svg" alt="star-icon" />
+                    <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
+                </div>
+
+                <span>⦿</span>
+                <p className="lang">{original_language}</p>
+                
+                <span>⦿</span>
+                <p className="date text-white">{release_date}</p>
+                <span>⦿</span>
+                <p className="overview text-white">{overview}</p>   
+            </div>
+            
         </div>
     );
 }
